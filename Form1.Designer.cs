45b0edb79c345fa5ead452bd0023f4f616f1cce7
Maxym Fediw <362706764@GoTVDSB.ca>
2026-04-02 13:20:09 -0400

    Almost Done!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,29 +35,32 @@
             this.imgBox = new System.Windows.Forms.PictureBox();
             this.lblFahrenheit = new System.Windows.Forms.Label();
             this.lblCelsius = new System.Windows.Forms.Label();
+            this.radC = new System.Windows.Forms.RadioButton();
+            this.radF = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
             this.SuspendLayout();
             // 
             // txtFahrenheit
             // 
-            this.txtFahrenheit.Location = new System.Drawing.Point(329, 126);
+            this.txtFahrenheit.Location = new System.Drawing.Point(136, 12);
             this.txtFahrenheit.Name = "txtFahrenheit";
             this.txtFahrenheit.Size = new System.Drawing.Size(138, 20);
             this.txtFahrenheit.TabIndex = 0;
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(329, 322);
+            this.btnSubmit.Location = new System.Drawing.Point(82, 309);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(138, 49);
             this.btnSubmit.TabIndex = 1;
             this.btnSubmit.Text = "Enter";
             this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // lblInstructions
             // 
             this.lblInstructions.AutoSize = true;
-            this.lblInstructions.Location = new System.Drawing.Point(326, 63);
+            this.lblInstructions.Location = new System.Drawing.Point(79, 56);
             this.lblInstructions.Name = "lblInstructions";
             this.lblInstructions.Size = new System.Drawing.Size(141, 13);
             this.lblInstructions.TabIndex = 2;
@@ -65,7 +68,7 @@
             // 
             // txtCelsius
             // 
-            this.txtCelsius.Location = new System.Drawing.Point(329, 92);
+            this.txtCelsius.Location = new System.Drawing.Point(82, 88);
             this.txtCelsius.Name = "txtCelsius";
             this.txtCelsius.Size = new System.Drawing.Size(138, 20);
             this.txtCelsius.TabIndex = 3;
@@ -73,7 +76,7 @@
             // imgBox
             // 
             this.imgBox.Image = global::_9_12_GUI_Summative.Properties.Resources.twentyFiveTo40;
-            this.imgBox.Location = new System.Drawing.Point(275, 156);
+            this.imgBox.Location = new System.Drawing.Point(31, 140);
             this.imgBox.Name = "imgBox";
             this.imgBox.Size = new System.Drawing.Size(243, 147);
             this.imgBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -85,7 +88,7 @@
             // lblFahrenheit
             // 
             this.lblFahrenheit.AutoSize = true;
-            this.lblFahrenheit.Location = new System.Drawing.Point(472, 129);
+            this.lblFahrenheit.Location = new System.Drawing.Point(280, 19);
             this.lblFahrenheit.Name = "lblFahrenheit";
             this.lblFahrenheit.Size = new System.Drawing.Size(17, 13);
             this.lblFahrenheit.TabIndex = 5;
@@ -94,17 +97,41 @@
             // lblCelsius
             // 
             this.lblCelsius.AutoSize = true;
-            this.lblCelsius.Location = new System.Drawing.Point(472, 95);
+            this.lblCelsius.Location = new System.Drawing.Point(226, 91);
             this.lblCelsius.Name = "lblCelsius";
             this.lblCelsius.Size = new System.Drawing.Size(18, 13);
             this.lblCelsius.TabIndex = 6;
             this.lblCelsius.Text = "°C";
             // 
+            // radC
+            // 
+            this.radC.AutoSize = true;
+            this.radC.Location = new System.Drawing.Point(119, 117);
+            this.radC.Name = "radC";
+            this.radC.Size = new System.Drawing.Size(36, 17);
+            this.radC.TabIndex = 7;
+            this.radC.TabStop = true;
+            this.radC.Text = "°C";
+            this.radC.UseVisualStyleBackColor = true;
+            // 
+            // radF
+            // 
+            this.radF.AutoSize = true;
+            this.radF.Location = new System.Drawing.Point(161, 117);
+            this.radF.Name = "radF";
+            this.radF.Size = new System.Drawing.Size(35, 17);
+            this.radF.TabIndex = 8;
+            this.radF.TabStop = true;
+            this.radF.Text = "°F";
+            this.radF.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(821, 553);
+            this.ClientSize = new System.Drawing.Size(303, 368);
+            this.Controls.Add(this.radF);
+            this.Controls.Add(this.radC);
             this.Controls.Add(this.lblCelsius);
             this.Controls.Add(this.lblFahrenheit);
             this.Controls.Add(this.imgBox);
@@ -129,6 +156,8 @@
         private System.Windows.Forms.PictureBox imgBox;
         private System.Windows.Forms.Label lblFahrenheit;
         private System.Windows.Forms.Label lblCelsius;
+        private System.Windows.Forms.RadioButton radC;
+        private System.Windows.Forms.RadioButton radF;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,14 +19,63 @@ namespace _9_12_GUI_Summative
 
         private void imgBox_Click(object sender, EventArgs e)
         {
-            int temperature;
+            
+        }
 
-            temperature = Convert.ToInt32(txtCelsius.Text);
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
 
+            string celsius, farenheit; //spelt "fahrenheit" differently so the code wouldn't spaz
+            int celcius, fahrenheit;
 
+            celsius = txtCelsius.Text;
+            farenheit = txtFahrenheit.Text;
 
+            Int32.TryParse(txtCelsius.Text, out celcius);
+            Int32.TryParse(txtFahrenheit.Text, out  fahrenheit);
 
+            imgBox.Visible = true;
+            
 
+            if (radC.Checked && celcius < 0 || radF.Checked && fahrenheit < 32)
+            {
+                
+                imgBox.Image = Properties.Resources.sub0;
+            }
+
+            
+
+            else if (celcius >= 0 && celcius < 25 || fahrenheit >= 32 && fahrenheit < 77)
+            {
+                
+                imgBox.Image = Properties.Resources.zeroTo25;
+            }
+
+            
+
+            else if (celcius >= 25 && celcius < 40 || fahrenheit >= 32 && fahrenheit < 104)
+            {
+                
+                imgBox.Image = Properties.Resources.twentyFiveTo40;
+            }
+
+            
+
+            else if (celcius >= 40 && celcius < 100 || fahrenheit >= 104 && fahrenheit < 212)
+            {
+                
+                imgBox.Image = Properties.Resources.fortyTo100;
+            }
+
+           
+
+            else if (celcius <= 100 || fahrenheit <= 212)
+            {
+                
+                imgBox.Image = Properties.Resources.over100;
+            }
+
+            
         }
     }
 }
